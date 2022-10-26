f39cc7f3254e312e05bc027775c4e36f2a501fa1
Joseph Lai <35094752+JosephLaiC@users.noreply.github.com>
2022-10-26 09:16:00 +0800

    Update del_mad_qc.py

--- del_mad_qc.py
+++ del_mad_qc.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
+
+'''
+Delete the mad_qc-related fileds, because croo cannot work correctly for some reason, if mad_qc is run.
+The only way that I can solve the bug is to delete the mad_qc-related filed.
+'''
+
+import json
+import argparse
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description = 'Delete mad_qc-related fields from metadata.json.')
+    parser.add_argument('-j', '--json', metavar = 'JSON', type = str, required = True, help = 'input metadata.json file')
+    parser.add_argument('-o', '--out_file', metavar = 'JSON', type = str, required = True, help = 'output revised metadata.json file')
+    args = parser.parse_args()
+    return args
+
+def main():
+    print('Start deleting ...')
+
 args = parse_arguments()
 
 with open(args.json, 'r') as ijf:
